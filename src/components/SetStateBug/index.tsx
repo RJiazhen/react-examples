ecfb889c3cc236523ae78db65a5dcb84dab20bd3
RJiazhen <jiazhen.ruan@outlook.com>
2024-07-29 08:16:12 +0800

    feat(components): simplify SetStateBug code

--- src/components/SetStateBug/index.tsx
+++ src/components/SetStateBug/index.tsx
@@ -1,6 +1,43 @@
-import { Home } from './components/home';
+import { useEffect, useState } from 'react';
 // https://juejin.cn/post/7233720373237776444
 
+export type Info = {
+  age?: number;
+  isLoggedIn?: boolean;
+};
+
+let normalObject = {};
+
 export const SetStateBug = () => {
-  return <Home />;
+  const [info, setInfo] = useState<Info>({});
+
+  useEffect(() => {
+    console.log('info updated', info);
+    console.log('normalObject', normalObject);
+  }, [info]);
+
+  const mergeInfo = (newInfo: Info) => {
+    console.log('info when merging', info);
+    setInfo({ ...info, ...newInfo });
+
+    console.log('normalObject when merging', normalObject);
+    normalObject = { ...normalObject, ...newInfo };
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      mergeInfo({ isLoggedIn: true });
+    }, 1000);
+
+    setTimeout(() => {
+      mergeInfo({ age: 40 });
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+      <p>age: {info.age} years old</p>
+      <p>isLoggedIn: {info?.isLoggedIn?.toString()}</p>
+    </>
+  );
 };
